1fed94fa42fb72956aeed3af28dbce3ba662ac35
Ubuntu <ubuntu@ip-172-31-57-149.ec2.internal>
2024-05-26 07:58:40 +0000

    changes

--- model.py
+++ model.py
@@ -2,6 +2,47 @@ import ollama
 import numpy as np
 import pandas as pd
 
-def create_scores_df(mentee_df, mentor_df):
-    #setting a scores matrix 
-    scores_df = pd.DataFrame(columns=mentee_df)
+pd.set_option('future.no_silent_downcasting', True)
+from variables import compatibility_scores
+
+def create_scores_df(mentee_id_list, mentor_id_list):
+    scores_df = pd.DataFrame(columns=mentee_id_list, index=mentor_id_list)
+    scores_df = scores_df.fillna(0)
+
+    #making sure that all values in the matrix are floats
+    scores_df = scores_df.astype(float)
+
+    return scores_df
+
+def compare_programs(scores_df, mentee_df, mentor_df, mentee_id_list, mentor_id_list):
+    for mentor_id in mentor_id_list:
+        for mentee_id in mentee_id_list:
+
+            mentor_program = dict(mentor_df.loc[mentor_id])['program']
+            mentee_program = dict(mentee_df.loc[mentee_id])['program']
+            
+            scores_df.loc[mentor_id, mentee_id] = round(compatibility_scores[mentor_program][mentee_program] * 0.2,2)
+
+
+def match():
+
+    mentee_df = pd.read_csv('csv/mentee.csv')
+    mentor_df = pd.read_csv('csv/mentor.csv')
+
+    mentee_id_list = list(mentee_df['id'])
+    mentor_id_list = list(mentor_df['id'])
+
+    mentor_df.set_index('id', inplace=True)
+    mentee_df.set_index('id', inplace=True)
+
+    scores_matrix_df = create_scores_df(mentee_id_list, mentor_id_list)
+    compare_programs(scores_matrix_df, mentee_df, mentor_df, mentee_id_list, mentor_id_list)
+
+    scores_matrix_df.to_csv('scores_df.csv')
+
+
+if __name__ == '__main__':
+    match()
+
+
+
---
+++ variables.py
@@ -0,0 +1,102 @@
+compatibility_scores = {
+    'Psychology': {
+        'Psychology': 1.0,
+        'Engineering': 0.1,
+        'Computer Science': 0.2,
+        'Chemistry': 0.1,
+        'English': 0.7,
+        'Biology': 0.3,
+        'Business': 0.3,
+        'Kinesiology': 0.5,
+        'Art History': 0.2
+    },
+    'Engineering': {
+        'Psychology': 0.1,
+        'Engineering': 1.0,
+        'Computer Science': 0.9,
+        'Chemistry': 0.5,
+        'English': 0.1,
+        'Biology': 0.4,
+        'Business': 0.5,
+        'Kinesiology': 0.4,
+        'Art History': 0.1
+    },
+    'Computer Science': {
+        'Psychology': 0.2,
+        'Engineering': 0.9,
+        'Computer Science': 1.0,
+        'Chemistry': 0.3,
+        'English': 0.2,
+        'Biology': 0.5,
+        'Business': 0.8,
+        'Kinesiology': 0.3,
+        'Art History': 0.1
+    },
+    'Chemistry': {
+        'Psychology': 0.1,
+        'Engineering': 0.5,
+        'Computer Science': 0.3,
+        'Chemistry': 1.0,
+        'English': 0.2,
+        'Biology': 0.8,
+        'Business': 0.4,
+        'Kinesiology': 0.5,
+        'Art History': 0.1
+    },
+    'English': {
+        'Psychology': 0.7,
+        'Engineering': 0.1,
+        'Computer Science': 0.2,
+        'Chemistry': 0.2,
+        'English': 1.0,
+        'Biology': 0.2,
+        'Business': 0.6,
+        'Kinesiology': 0.3,
+        'Art History': 0.9
+    },
+    'Biology': {
+        'Psychology': 0.3,
+        'Engineering': 0.4,
+        'Computer Science': 0.5,
+        'Chemistry': 0.8,
+        'English': 0.2,
+        'Biology': 1.0,
+        'Business': 0.4,
+        'Kinesiology': 0.6,
+        'Art History': 0.2
+    },
+    'Business': {
+        'Psychology': 0.3,
+        'Engineering': 0.5,
+        'Computer Science': 0.8,
+        'Chemistry': 0.4,
+        'English': 0.6,
+        'Biology': 0.4,
+        'Business': 1.0,
+        'Kinesiology': 0.3,
+        'Art History': 0.2
+    },
+    'Kinesiology': {
+        'Psychology': 0.5,
+        'Engineering': 0.4,
+        'Computer Science': 0.3,
+        'Chemistry': 0.5,
+        'English': 0.3,
+        'Biology': 0.6,
+        'Business': 0.3,
+        'Kinesiology': 1.0,
+        'Art History': 0.2
+    },
+    'Art History': {
+        'Psychology': 0.2,
+        'Engineering': 0.1,
+        'Computer Science': 0.1,
+        'Chemistry': 0.1,
+        'English': 0.9,
+        'Biology': 0.2,
+        'Business': 0.2,
+        'Kinesiology': 0.2,
+        'Art History': 1.0
+    }
+}
+
