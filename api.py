e44dd5343e6df328dcb3f11fb35c2c5d078b641d
Ubuntu <ubuntu@ip-172-31-57-149.ec2.internal>
2024-05-28 05:01:47 +0000

    changes, added gitignore

--- api.py
+++ api.py
@@ -1,8 +1,14 @@
-# from quart import Quart, jsonify, request
-# from quart_cors import cors
-# import json
-# import pandas as pd
+from quart import Quart, jsonify, request
+#from quart_cors import cors
+import json
+import pandas as pd
 from model import matching_scores
 
-if __name__ == '__main__':
-    matching_scores()
+#export QUART_APP=api:app
+#quart run
+
+app = Quart(__name__)
+
+# @app.route("/")
+# async def index() -> str:
+#     return 'hello'
