6e173cbadb369360a835ec27be46a03f347af8ae
bigriceman0128 <112298940+ardenmonaghan@users.noreply.github.com>
2024-06-06 01:29:40 -0600

    changes

--- api.py
+++ api.py
@@ -1,32 +1,35 @@
-from quart import Quart, jsonify, request
-#from quart_cors import cors
+from quart import Quart, jsonify, request, abort
+# from quart_cors import cors
+# from quart_auth import AuthManager, login_user, logut_user, current_user, login_required, User
+
 import json
 import pandas as pd
 from model import matching_scores
 
-#export QUART_APP=api:app
-#quart run
-#source mentor_env/bin/activate
+# export QUART_APP=api:app
+# quart run
+# source mentor_env/bin/activate
 app = Quart(__name__)
+
 
 @app.post("/csv")
 async def csv_intake():
-    data = await request.get_json() #dictionary of json {'mentee': json, mentor: 'json'} 
+    # dictionary of json {'mentee': json, mentor: 'json'}
+    data = await request.get_json()
     print(type(data['mentee']))
-    mentee_df = pd.read_json(data['mentee'],orient='records')
-    mentor_df = pd.read_json(data['mentor'],orient='records')
+    mentee_df = pd.read_json(data['mentee'], orient='records')
+    mentor_df = pd.read_json(data['mentor'], orient='records')
 
     if mentee_df is None or mentor_df is None:
-        abort(400, description="The mentor data or mentee data was not succesfully recieved")
+        abort(
+            400, description="The mentor data or mentee data was not succesfully recieved")
 
     scores_df_json = matching_scores(mentee_df, mentor_df)
 
-    response_data = {'message': 'Data Recieved Sucessfully', 'scores': scores_df_json}
+    response_data = {'message': 'Data Recieved Sucessfully',
+                     'scores': scores_df_json}
     return jsonify(response_data), 200
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
-
-
-    
-
+    # current port 5001
+    app.run(host='0.0.0.0', port=5001)
--- model.py
+++ model.py
@@ -1,10 +1,11 @@
+from variables import compatibility_scores
 import ollama
 import numpy as np
 import pandas as pd
 from sklearn.metrics.pairwise import cosine_similarity
 
 pd.set_option('future.no_silent_downcasting', True)
-from variables import compatibility_scores
+
 
 def create_scores_df(mentee_id_list, mentor_id_list):
     '''creating a dataframe matrix of scores
@@ -16,10 +17,11 @@ def create_scores_df(mentee_id_list, mentor_id_list):
     scores_df = pd.DataFrame(columns=mentee_id_list, index=mentor_id_list)
     scores_df = scores_df.fillna(0)
 
-    #making sure that all values in the matrix are floats
+    # making sure that all values in the matrix are floats
     scores_df = scores_df.astype(float)
 
     return scores_df
+
 
 def compare_programs(scores_df, mentee_df, mentor_df, mentee_id_list, mentor_id_list):
     '''
@@ -36,8 +38,10 @@ def compare_programs(scores_df, mentee_df, mentor_df, mentee_id_list, mentor_id_
 
             mentor_program = dict(mentor_df.loc[mentor_id])['program']
             mentee_program = dict(mentee_df.loc[mentee_id])['program']
-            
-            scores_df.loc[mentor_id, mentee_id] = round((compatibility_scores[mentor_program][mentee_program] * 0.4) + scores_df.loc[mentor_id, mentee_id],5)
+
+            scores_df.loc[mentor_id, mentee_id] = round(
+                (compatibility_scores[mentor_program][mentee_program] * 0.4) + scores_df.loc[mentor_id, mentee_id], 5)
+
 
 def embed_interests(interests):
     for i in range(len(interests)):
@@ -45,6 +49,7 @@ def embed_interests(interests):
             model='mxbai-embed-large', prompt=interests[i])['embedding']
 
     return np.array(interests)
+
 
 def compare_interests(scores_df, mentee_df, mentor_df, mentee_id_list, mentor_id_list):
 
@@ -54,16 +59,16 @@ def compare_interests(scores_df, mentee_df, mentor_df, mentee_id_list, mentor_id
     embedded_mentor = embed_interests(list(mentor_df['interests']))
     embedded_mentee = embed_interests(list(mentee_df['interests']))
 
-    #the returned similarity matrix will have the first index 0 be the first row, and it rhe first index will contain all of the scores for all of the mentees
+    # the returned similarity matrix will have the first index 0 be the first row, and it rhe first index will contain all of the scores for all of the mentees
     # matched to that mentor in the row
     similarity_matrix = cosine_similarity(embedded_mentor, embedded_mentee)
 
     print(similarity_matrix)
 
-
-    for i,mentor_id in enumerate(mentor_id_list):
-        for j,mentee_id in enumerate(mentee_id_list):
-            scores_df.loc[mentor_id, mentee_id] = round((similarity_matrix[i][j] * 0.4) + scores_df.loc[mentor_id, mentee_id] ,5)
+    for i, mentor_id in enumerate(mentor_id_list):
+        for j, mentee_id in enumerate(mentee_id_list):
+            scores_df.loc[mentor_id, mentee_id] = round(
+                (similarity_matrix[i][j] * 0.4) + scores_df.loc[mentor_id, mentee_id], 5)
 
 
 def compare_static(scores_df, mentee_df, mentor_df, mentee_id_list, mentor_id_list, retrieve, weighting):
@@ -75,40 +80,39 @@ def compare_static(scores_df, mentee_df, mentor_df, mentee_id_list, mentor_id_li
             mentor_column = dict(mentor_df.loc[mentor_id])[retrieve]
             mentee_column = dict(mentee_df.loc[mentee_id])[retrieve]
 
-            if(mentor_column == mentee_column):
-                scores_df.loc[mentor_id, mentee_id] += round(weighting,2)
-                
+            if (mentor_column == mentee_column):
+                scores_df.loc[mentor_id, mentee_id] += round(weighting, 2)
 
-def matching_scores(mentee_df,mentor_df):
 
-    #mentee_df = pd.read_csv('csv/mentee.csv')
-    #mentor_df = pd.read_csv('csv/mentor.csv')
+def matching_scores(mentee_df, mentor_df):
 
-    #retrieve all of the mentors and mentees in seperate lists
-    mentee_id_list = list(mentee_df['id']) 
-    mentor_id_list = list(mentor_df['id']) 
+    # retrieve all of the mentors and mentees in seperate lists
+    mentee_id_list = list(mentee_df['id'])
+    mentor_id_list = list(mentor_df['id'])
 
-    #removing index 
+    # removing index
     mentor_df.set_index('id', inplace=True)
     mentee_df.set_index('id', inplace=True)
 
     scores_matrix_df = create_scores_df(mentee_id_list, mentor_id_list)
 
-    #Comparing mentors to mentees to get all the scores
-    compare_programs(scores_matrix_df, mentee_df, mentor_df, mentee_id_list, mentor_id_list)
-    compare_static(scores_matrix_df, mentee_df, mentor_df, mentee_id_list, mentor_id_list, 'residence', 0.2)
-    #compare_static(scores_matrix_df, mentee_df, mentor_df, mentee_id_list, mentor_id_list, 'international', 0.2)
-    compare_interests(scores_matrix_df, mentee_df, mentor_df, mentee_id_list, mentor_id_list)
+    # Comparing mentors to mentees to get all the scores
+    compare_programs(scores_matrix_df, mentee_df, mentor_df,
+                     mentee_id_list, mentor_id_list)
+    compare_static(scores_matrix_df, mentee_df, mentor_df,
+                   mentee_id_list, mentor_id_list, 'residence', 0.2)
+    # compare_static(scores_matrix_df, mentee_df, mentor_df, mentee_id_list, mentor_id_list, 'international', 0.2)
+    compare_interests(scores_matrix_df, mentee_df, mentor_df,
+                      mentee_id_list, mentor_id_list)
 
     return scores_matrix_df.to_json()
 
 # assigning mentees to specific mentors
-def assign():
+
+
+def assign(scores_matrix_df, mentee_df, mentor_df):
     pass
 
 
 if __name__ == '__main__':
     matching_scores()
-
-
-
